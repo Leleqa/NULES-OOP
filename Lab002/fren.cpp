052ed017b9c79b2dd6cb7d48828de4958828de2c
Leleqa <KiteNock@protonmail.com>
2023-03-08 22:45:43 +0200

    Lab2

--- Lab002/fren.cpp
+++ Lab002/fren.cpp
@@ -3,50 +3,60 @@
 
 using namespace std;
 
-class triangle{
-    float Aside, Bside, Cside;
+class HEXAGON {
+  float a;
 
-    public:
-    triangle();
-    triangle(float, float, float);
-    void sside(float, float, float);
-    void gside();
+public:
+    HEXAGON();
+    HEXAGON(float);
+    void sside(float);
+    float gside();
     float area();
+  friend HEXAGON merge_pucks(HEXAGON, HEXAGON);
 };
-triangle::triangle(){
-    Aside=Bside=Cside=0;
+HEXAGON::HEXAGON(){
+    a=0;
     cout <<  "Конструктор викликаний для об'єкта " << this << endl;
 }
-triangle::triangle(float ASIDE, float BSIDE, float CSIDE){
-    Aside = ASIDE;
-    Bside = BSIDE;
-    Cside = CSIDE;
+
+HEXAGON::HEXAGON(float ASIDE){
+    a = ASIDE;
     cout << "Конструктор з параметрами викликаний для об'єкта " << this << endl;
 }
-void triangle::sside(float ASIDE, float BSIDE, float CSIDE){
-    Aside = ASIDE;
-    Bside = BSIDE;
-    Cside = CSIDE;
-    cout << "Сторони трикутника перезадано для об'єкта " << this << endl;
+
+void HEXAGON::sside(float ASIDE){
+    a = ASIDE;
+    cout << "Сторону шайби перезадано для об'єкта " << this << endl;
 }
-void triangle::gside(){
-    cout << "Сторона A: " << Aside << " Сторона B: " << Bside << " Сторона C: " << Cside << " " << this << endl;
+
+float HEXAGON::gside(){
+    return a;
 }
-float triangle::area(){
-    return sqrtf((Aside+Bside+Cside)*(Bside+Cside-Aside)*(Aside+Cside-Bside)*(Aside+Bside-Cside))/4; 
+float HEXAGON::area(){
+    return ((3*sqrtf(3))/2)*pow(a,2);
     }
 
-void print_sqr(triangle &trianp, triangle &trian){
-    trianp.gside();
-    cout << "Площа трикутника з заданими пaрaметрами = " << trianp.area() << endl;
-    trian.sside(4, 5, 6);
+void print_sqr(HEXAGON &puck1, HEXAGON &puck2){
+    cout << "Сторона шайби: " << puck1.gside() << endl;
+    cout << "Площа шайби з заданими пaрaметрами = " << puck1.area() << endl;
+    puck2.sside(4);
+}
+
+  HEXAGON merge_pucks(HEXAGON puck1, HEXAGON puck2){
+  HEXAGON puck3;
+  float area = ((3*sqrtf(3))/2)*pow(puck1.a,2) + ((3*sqrtf(3))/2)*pow(puck2.a,2); // sum of puck1.a and puck2.a
+  puck3.a = pow(3,(1/4))*sqrtf(((2*area)/9));
+  return puck3;
+
 }
 
 int main(){
-    triangle triplet;
-    triangle tripletp(10,7,5);
-    print_sqr(tripletp, triplet);
-    triplet.gside();
-
-    return 0;
+  HEXAGON puck1(8);
+  HEXAGON puck2;
+  HEXAGON puck3;
+  print_sqr(puck1, puck2);
+  cout << puck2.area() << endl;
+  puck3 = merge_pucks(puck1,puck2);
+  cout << "Сторона нової шайби: " << puck3.gside() << endl;
+  return 0;
 }
