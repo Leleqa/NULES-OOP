fb3ccad6af700020063c61a08a16f72703830ac9
Leleqa <KiteNock@protonmail.com>
2023-03-10 19:09:35 +0200

    Finalised (hopefuly) Lab3

--- Lab003/classy-files.cpp
+++ Lab003/classy-files.cpp
@@ -30,37 +30,71 @@ public:
   }
 };
 
-
-int main(){
-  ifstream file;
-  string str;
-  int lineCount = 0;
-  
-  file.open("group.txt");
-
-  if(file){
+int CountLine(ifstream &file){
+int lineCount = 0;
+string str;
+if(file){
     while(!file.eof()){
       getline(file, str, '\n');
       lineCount++;
     }
   lineCount--;
+    return lineCount;
+  }
+else{
+      cout << "Не вдалося відкрити файл" << endl; 
+      return -1;
+    }
+}
 
+void WriteObjects(STUDENT *group, ifstream &file, int lineCount){
+  
+  int grade1, grade2, grade3;
+  string name;
+ for (int i = 0; i < lineCount; i++){
+
+     file >> name;
+     file >> grade1;
+     file >> grade2;
+     file >> grade3;
+     group[i].Set(name, grade1,grade2,grade3);
+   }
+}
+void ReadAllObj(STUDENT *group, int lineCount){
+ for(int i = 0; i < lineCount; i++){
+      cout << group[i].GetName() << " "<< group[i].GetGradeA() << " " << group[i].GetGradeB() << " " << group[i].GetGradeC() << endl;
+
+    }
+}
+
+void ReadSomeObj(STUDENT *group, int lineCount){
+  for(int i = 0; i < lineCount; i++){
+    if((group[i].GetGradeA() != 3) && (group[i].GetGradeB()  != 3) && (group[i].GetGradeC() != 3) && (group[i].GetGradeA() != 2) && (group[i].GetGradeB() != 2) && (group[i].GetGradeC() != 2)){
+      cout << group[i].GetName() << " "<< group[i].GetGradeA() << " " << group[i].GetGradeB() << " " << group[i].GetGradeC() << endl;
+    }
+    }
+  }
+
+
+
+
+int main(){
+  ifstream file;
+  int lineCount;
+  
+  file.open("group.txt");
+  if(file){
+  lineCount = CountLine(file);
+  
   STUDENT *group = new STUDENT[lineCount];
   file.clear();
   file.seekg(0);
   
-  for (int i; i < lineCount; i++){
-      string name;
-      int grade1, grade2, grade3;
-      file >> name;
-      file >> grade1;
-      file >> grade2;
-      file >> grade3;
-      group[i].Set(name, grade1,grade2,grade3);
-    }
-  for(int i = 0; i < lineCount; i++){
-      cout << group[i].GetName() << " "<< group[i].GetGradeA() << " " << group[i].GetGradeB() << " " << group[i].GetGradeC() << endl;
+  WriteObjects(group, file, lineCount);
 
-    }
+  ReadAllObj(group, lineCount);
+  cout << "\n" << endl;
+  ReadSomeObj(group, lineCount);
 
-  }}
+  }
+}
